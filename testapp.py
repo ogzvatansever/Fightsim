c1501009fb4379d0e7a083faa1aab4eaae0819ca
Oguz Vatansever <ogzvatansever@gmail.com>
2022-08-21 10:37:30 +0300

    Testing a fun project

---
+++ testapp.py
@@ -0,0 +1,7 @@
+class Fighter :
+
+    def __init__(self, nickname, agility, strength, stamina,) :
+        self.nickname = nickname
+        self.agility = 1.0 + agility
+        self.strength = 1.0 + strength
+        self.stamina = 1.0 + stamina
