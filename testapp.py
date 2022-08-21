f2d7f8ae73cf6219fba7b3ab583a41e0129daccd
Oguz Vatansever <ogzvatansever@gmail.com>
2022-08-21 12:31:58 +0300

    Made a basic fighter class

--- testapp.py
+++ testapp.py
@@ -1,4 +1,7 @@
-#Test app currently
+import random
+
+# Will make a fighter database later
+
 class Fighter :
 
     def __init__(self, nickname, agility, strength, stamina,) :
@@ -6,3 +9,6 @@ class Fighter :
         self.agility = 1.0 + agility
         self.strength = 1.0 + strength
         self.stamina = 1.0 + stamina
+
+    def printStats(self):
+        print(self.nickname,self.agility,self.strength,self.stamina)
