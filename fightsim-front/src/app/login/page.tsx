25acadd1431535959b202dbd27901acba2d2f387
Oguz Vatansever <ogzvatansever@gmail.com>
2025-05-12 00:22:40 +0300

    refactor: remove unused ThemeProvider import from LoginPage

--- fightsim-front/src/app/login/page.tsx
+++ fightsim-front/src/app/login/page.tsx
@@ -1,5 +1,4 @@
 import { LoginForm } from "@/components/login-form"
-import { ThemeProvider } from "@/components/theme-provider"
 import { Navigate } from "react-router"
 
 export default function LoginPage() {
